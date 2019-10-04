18ac6db48c374ab86334b8aff4cf696184b95466
ibrahim <ibrahim.k@pogeyan.com>
2019-10-04 19:41:21 +0530

    load initial data

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,42 +6,50 @@ const twitter = require("./twitter-data.json");
 
 class SearchBar extends Component {
 
-    componentWillMount() {
-        this.props.loadInitalData({ twitterData: twitter })
-    }
+	componentWillMount() {
+		this.props.loadInitalData({ twitterData: twitter })
+	}
 
-    onSearchClick = () => {
-        const { value, twitterData } = this.props;
-        this.props.onSearchKeyword(value, twitterData);
-    }
+	onSearchClick = () => {
+		const { value, twitterData } = this.props;
+		this.props.onSearchKeyword(value, twitterData);
+	}
 
 
-    render() {
-        const { value, searchedTweet } = this.props;
-        return <div id="search-bar" className="container-fluid">
-            <div className="row">
-                <input class="form-control form-control-lg" onChange={e => this.props.onHandleChange(e.target.value)} value={value} type="text" id="search" placeholder="Enter a Keyword"></input>
-                <button type="button" onClick={this.onSearchClick} class="btn btn-primary">Search</button>
-                {searchedTweet && < Tweet searchedTweet={searchedTweet} />}
-            </div>
-        </div>;
-    }
+	render() {
+		const { value, searchedTweet } = this.props;
+		return <div
+		><h1 style={{ "padding-left": "25%" }}>Twitter Search </h1>
+			<div className="container">
+				<div>
+					<div className="row">
+						<div id="search-bar" className="col-sm" >
+							<input className="form-control form-control-lg" onChange={e => this.props.onHandleChange(e.target.value)} value={value} type="text" id="search" placeholder="Enter a Keyword"></input>
+						</div>
+						<div className="col-sm">
+							<button type="button" onClick={this.onSearchClick} className="btn btn-primary">Search</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			{searchedTweet && < Tweet searchedTweet={searchedTweet} />}
+		</div>;
+	}
 
 }
 
 const mapStateToProps = (state) => {
-    return {
-        value: state.value,
-        twitterData: state.twitterData,
-        searchedTweet: state.searchedTweet
-    };
+	return {
+		value: state.value,
+		twitterData: state.twitterData,
+		searchedTweet: state.searchedTweet
+	};
 };
 const mapDispatchToprops = (dispatch) => {
-    return {
-        onHandleChange: (value) => dispatch(doHandleChange(value)),
-        loadInitalData: ({ twitterData }) => dispatch(doLoadInitialData(twitterData)),
-        onSearchKeyword: (searchKey, twitterData) => dispatch(doSearchKeyword(searchKey, twitterData)),
-    }
+	return {
+		onHandleChange: (value) => dispatch(doHandleChange(value)),
+		loadInitalData: ({ twitterData }) => dispatch(doLoadInitialData(twitterData)),
+		onSearchKeyword: (searchKey, twitterData) => dispatch(doSearchKeyword(searchKey, twitterData)),
+	}
 }
 export default connect(mapStateToProps, mapDispatchToprops)(SearchBar)
-// connect(maPStateToProps, mapDispatchToprops)(Basic)
--- src/components/TweetComponent/Tweet.js
+++ src/components/TweetComponent/Tweet.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
 
 class Tweet extends Component {
 
 
-    onSearchClick = () => {
-        const { value, twitterData } = this.props;
-        this.props.onSearchKeyword(value, twitterData);
-    }
+	render() {
 
-
-    render() {
-        const { searchedTweet } = this.props;
-        return <div id="tweet" className="col-sm-8">
-            {searchedTweet.map(t => <h>{t.text}</h>)}
-        </div>;
-    }
+		const { searchedTweet } = this.props;
+		return <div id="tweet"  >
+			{searchedTweet.map(t => <div id={t.id} style={{ "margin": "2%", "padding": "2%", "height": "auto", "width": "90%", "border": "3px solid skyblue" }}>
+				<div className="profile">
+					{/* TODO : need to implement user information with logo of the user */}
+				</div>
+				{t.text}</div>)}
+		</div>;
+	}
 
 }
 export default Tweet;
